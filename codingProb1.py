422193e4e2bdc414c9d0618f5fae31f61d9770b7
Wayne Swamber Jr <70187919+swamberwayne@users.noreply.github.com>
2020-08-26 14:30:57 -0500

    Create codingProb1.py

---
+++ codingProb1.py
@@ -0,0 +1,8 @@
+# Wayne Swamber Jr
+# PSID: 1419882
+# Calculate the age of person based on current date and the birthday of the user.
+# The program should:
+# Prompt the user to enter the current date by month, day and year
+# Prompt the user to enter the birthday by month, day and year
+# Output the age of the user in years
+# Output a "Happy Birthday!" message if the current date is the user's actual birthday.
