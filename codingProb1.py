92ad580754ff110c45c12b169b23a637c9047d6e
swamberwayne <wswamber@uh.edu>
2020-08-26 20:05:49 -0500

    added code

--- codingProb1.py
+++ codingProb1.py
@@ -6,3 +6,26 @@
 # Prompt the user to enter the birthday by month, day and year
 # Output the age of the user in years
 # Output a "Happy Birthday!" message if the current date is the user's actual birthday.
+
+print("Birthday Calculator")
+print("Current day")
+
+cd_month = int(input("Month: "))
+cd_day = int(input("Day: "))
+cd_year = int(input("Year: "))
+
+print("Birthday")
+
+bd_month = int(input("Month: "))
+bd_day = int(input("Day: "))
+bd_year = int(input("Year: "))
+
+age = cd_year - bd_year
+
+print("You are", age, "years old.")
+
+
+
+
+
+
