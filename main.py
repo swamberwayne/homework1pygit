c2c7da888a797e0910243d7fb677f89c47decd61
swamberwayne <wswamber@uh.edu>
2020-09-04 01:49:36 -0500

    code added

---
+++ main.py
@@ -0,0 +1,13 @@
+# Wayne Swamber
+# 1419882
+
+user_num = int(input('Enter integer:\n'))
+
+print("You entered:", user_num)
+print(user_num, "squared is", user_num * user_num)
+print("And", user_num, "cubed is", user_num * user_num * user_num, "!!")
+
+user_num2 = int(input('Enter another integer:\n'))
+
+print(user_num, "+", user_num2, "is", user_num + user_num2)
+print(user_num, "*", user_num2, "is", user_num * user_num2)
